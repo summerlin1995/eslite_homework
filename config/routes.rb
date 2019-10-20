12035b9d63b9e6bae21c5e4f4707715b8fce5284
summerlin <summerchihuilin@gmail.com>
2019-10-20 11:06:46 +0800

    device gem done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :products, only: [:index, :new, :edit, :create, :show] do
+      resources :product_photos, only: [:index, :create]
+    end
+  end
 end
